38ab5b8fb8bbc2e9e27ec2d457795a1d730b8145
Andrew Gerez <andrewgerez18@gmail.com>
2024-02-04 18:01:26 -0300

    feat: react & taiwlind setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
+import installedIntegration from '@astrojs/react';
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [installedIntegration(), react(), tailwind()]
+});
